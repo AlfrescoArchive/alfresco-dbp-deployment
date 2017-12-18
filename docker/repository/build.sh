e8427329d839548d39a5490090e7ce9be813d9cb
Gavin Cornwell <gcornwell@gmail.com>
2017-12-18 19:14:58 +0000

    DEPLOY-277 / DEPLOY-269: Deployment of the RM and Sync Services amps
    Updated docker build command.

--- docker/repository/build.sh
+++ docker/repository/build.sh
@@ -7,4 +7,4 @@ echo "Downloading required AMPs..."
 mvn clean package
 
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."
-docker build . -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .

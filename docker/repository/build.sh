969444124a3cbb4c5e2d98872a6ef158bfe70f32
Gavin Cornwell <gcornwell@gmail.com>
2017-12-18 17:56:49 +0000

    DEPLOY-277 / DEPLOY-269: Deployment of the RM and Sync Services amps
    Introduced separate repo and share folders to build DBP docker images using local script.

---
+++ docker/repository/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -o errexit
+
+. ./build.properties
+
+echo "Downloading required AMPs..."
+mvn clean package
+
+echo "Building image..."
+docker build . -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG

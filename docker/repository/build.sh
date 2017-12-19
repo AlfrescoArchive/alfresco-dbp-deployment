3d15bc693a3a8f487bb0214e6164697b8a999788
Gavin Cornwell <gcornwell@gmail.com>
2017-12-19 09:37:42 +0000

    DEPLOY-277 / DEPLOY-269: Deployment of the RM and Sync Services amps
    Provide full path to mvn executable.

--- docker/repository/build.sh
+++ docker/repository/build.sh
@@ -7,7 +7,7 @@ echo "Downloading required AMPs..."
 if [[ "$bamboo_capability_system_builder_mvn3_Maven_3_3" == "" ]]; then
 	mvn clean package
 else
-    $bamboo_capability_system_builder_mvn3_Maven_3_3 clean package
+    $bamboo_capability_system_builder_mvn3_Maven_3_3/bin/mvn clean package
 fi
 
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."

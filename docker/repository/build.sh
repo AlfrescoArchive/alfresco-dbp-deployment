2f05a2fa7eff92392169476f25828c0037bd4631
Gavin Cornwell <gcornwell@gmail.com>
2017-12-19 09:31:15 +0000

    DEPLOY-277 / DEPLOY-269: Deployment of the RM and Sync Services amps
    Workaround for messy maven environment on build agents.

--- docker/repository/build.sh
+++ docker/repository/build.sh
@@ -4,7 +4,11 @@ set -o errexit
 . ./build.properties
 
 echo "Downloading required AMPs..."
-mvn clean package
+if [[ "$bamboo_capability_system_builder_mvn3_Maven_3_3" == "" ]]; then
+	mvn clean package
+else
+    $bamboo_capability_system_builder_mvn3_Maven_3_3 clean package
+fi
 
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."
 docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .

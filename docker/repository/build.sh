4449058839a5e47449f0cd4147725d47b8aa10eb
Ramona Popa <ramona.popa@ness.com>
2019-06-12 14:18:10 +0300

    Auth 288: Create new image for ACS with SAML enabled (#110)
       - added SAML 1.1.1, gs 3.1-SNAPSHOT and sync service 3.1.0
       - use new images with amps applied

--- docker/repository/build.sh
+++ docker/repository/build.sh
@@ -6,13 +6,12 @@ set -o errexit
 # To support local and Bamboo builds only call mvn when the target folder is missing.
 # To do a full clean build locally run "mvn clean" before running this script.
 
-# Commented as part of DEPLOY-277 / DEPLOY-269: Deployment of the RM and Sync Services amps
-#if [ -d ./target ]; then
-#    echo "Required AMPs already present!"
-#else
-#    echo "Downloading required AMPs..."
-#    mvn clean package
-#fi
+if [ -d ./target ]; then
+    echo "Required AMPs already present!"
+else
+    echo "Downloading required AMPs..."
+    mvn clean package
+fi
 
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."
 docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .

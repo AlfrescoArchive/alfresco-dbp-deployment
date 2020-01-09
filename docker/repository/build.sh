a55e84791a264cde8ab8fa518c48c3864551b9ce
Cristina Sirbu <cristina.sirbu@ness.com>
2020-01-09 15:47:07 +0200

    DEPLOY-915: Fix Build of DBP Repo and Share Docker Images Typo

--- docker/repository/build.sh
+++ docker/repository/build.sh
@@ -19,4 +19,4 @@ then
 fi
 
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."
-#docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .

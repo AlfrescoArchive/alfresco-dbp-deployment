8d0f3660929f1f050d2a62e44efc1413e5139761
Cristina Sirbu <cristina.sirbu@ness.com>
2020-01-14 14:01:03 +0200

    Deploy 915: Fix Build of DBP Repo and Share Docker Images (#149)
    * DEPLOY-915: Fix Build of DBP Repo and Share Docker Images
    Added support for building repo and share images with branch name tags
    Add 1.6.x and develop branch to if in build and push

--- docker/share/build.sh
+++ docker/share/build.sh
@@ -12,5 +12,10 @@ else
     mvn clean package
 fi
 
+if [ ! -z "$BRANCH_NAME" -a "$BRANCH_NAME" != "master" -a "$BRANCH_NAME" != "1.6.x" -a "$BRANCH_NAME" != "develop" ]
+then
+  DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG}-${BRANCH_NAME}"
+fi
+
 echo "Building image ($DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG)..."
 docker build -t quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
--- docker/share/push.sh
+++ docker/share/push.sh
@@ -3,5 +3,10 @@ set -o errexit
 
 . ./build.properties
 
+if [ ! -z "$BRANCH_NAME" -a "$BRANCH_NAME" != "master" -a "$BRANCH_NAME" != "1.6.x" -a "$BRANCH_NAME" != "develop" ]
+then
+  DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG}-${BRANCH_NAME}"
+fi
+
 echo "Pushing image..."
 docker push quay.io/alfresco/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
